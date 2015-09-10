eeff6f4c848546d804ebb0a17ebbca54cc11e634
AbhishekKr <abhikumar163@gmail.com>
2015-09-11 01:45:45 +0530

    [splitter] human display of engine details

--- splitter/engines.go
+++ splitter/engines.go
@@ -3,6 +3,22 @@ package momentdbSplitter
 import "fmt"
 
 /*
+displayDetail to show engine internal mapping to split ends in human terms.
+*/
+func displayDetail(engineIndex int, engine EngineDetail) {
+	detail := fmt.Sprintf("[+] Engine#%d Detail\n", (engineIndex + 1))
+	detail = fmt.Sprintf("%s |  Source: %s:%v\n\n", detail, engine.SourceIP, engine.SourcePorts)
+	for destination_idx, destination := range engine.Destinations {
+		detail = fmt.Sprintf("%s |[+]  Destination#%d: %s:%v\n", detail, (destination_idx + 1), destination.DestinationIP, destination.DestinationPorts)
+		detail = fmt.Sprintf("%s | |     Split Mode: %v\n", detail, destination.SplitterMode)
+		detail = fmt.Sprintf("%s | |     Split Pattern: %v\n", detail, destination.SplitterPattern)
+		detail = fmt.Sprintf("%s | |     Split Type: %v\n", detail, destination.SplitterType)
+		detail = fmt.Sprintf("%s | |     Request Pattern: %v\n\n", detail, destination.RequestPattern)
+	}
+	fmt.Println(detail)
+}
+
+/*
 StartEngines will start all the goshare datastore engines based on config,
 if any engine failed, a failover local will be created and assigned
 */
@@ -10,9 +26,9 @@ func StartEngines(configPath string) {
 	usableEngines, _ := LoadEngineCollection(configPath)
 
 	fmt.Println("Start all the engines configured...")
-	for idx := range usableEngines.Engines {
-		fmt.Println(usableEngines.Engines[idx])
-		go ZmqSmartProxy(&(usableEngines.Engines[idx]))
+	for engineIndex := range usableEngines.Engines {
+		displayDetail(engineIndex, usableEngines.Engines[engineIndex])
+		go ZmqSmartProxy(&(usableEngines.Engines[engineIndex]))
 	}
 }
 
